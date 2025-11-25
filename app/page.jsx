6ed2130959cd242fff5c3e0da9d9b0fc84d30b01
aaronbogumil <aaronbogumil@gmail.com>
2025-11-25 12:00:16 +0000

    add responsive html

--- app/page.jsx
+++ app/page.jsx
@@ -3,27 +3,25 @@ import Link from "next/link";
 export default function Login() {
   return (
     <>
-      <form className="flex flex-col border-2 border-amber-600 rounded-lg p-2 items-center w-150 mx-auto mt-3">
-        <h1>Login here</h1>
-        <div className="grid grid-flow-col grid-rows-2 items-center">
-          <div className="grid grid-flow-row grid-cols-2">
-            <label className="indent-30 text-middle content-center">Username:</label>
-            <input className="p-2 m-1 border-2 rounded-lg text-left" name="username" id="username" />
+      <form className="grid-cols border-2 border-blue-400 rounded-lg p-2 items-center w-150 mx-auto mt-3 max-w-full h-auto">
+        <h1 className="p-7 text-center content-center">Login here</h1>
+        <div className="grid-rows-2 items-center">
+          <div className="grid grid-flow-row grid-cols-2 col grid-rows-1">
+            <label className="indent-20 text-middle content-center">Email:</label>
+            <input className="p-2 m-1 border-2 rounded-lg text-left" name="email" id="email" />
           </div>
           <div className="grid grid-flow-row grid-cols-2 ">
-            <label className="indent-30 text-middle content-center">Password:</label>
-            <input className="p-2 m-1 border-2 rounded-lg text-left" name="password" id="password" />
+            <label className="indent-20 text-middle content-center">Password:</label>
+            <input className="p-2 m-1 border-2 rounded-lg text-left" name="password" id="password" type="password" />
           </div>
         </div>
         <div>
-          <button className="bg-amber-600 rounded-lg p-3 m-2 hover:bg-amber-500">Login</button>
-          <Link className="bg-amber-600 rounded-lg p-3 m-2 hover:bg-amber-500" href="http://localhost:3000/sign-up">
-            Create an account{" "}
+          <button className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">Login</button>
+          <Link href="http://localhost:3000/sign-up">
+            <button className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">Create an account</button>
           </Link>
-      
         </div>
       </form>
-      
     </>
   );
 }
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -3,32 +3,37 @@ import Link from "next/link";
 export default function Signup() {
   return (
     <>
-      <form className="flex flex-col border-2 border-amber-600 rounded-lg p-2 items-center w-150 mx-auto mt-3">
-        <h1 className="p-7">Sign-up here</h1>
-        <div className="grid grid-flow-col grid-rows-7 items-center">
+      <form className="grid-cols border-2 border-blue-400 rounded-lg p-2 items-center w-150 mx-auto mt-3 max-w-full h-auto">
+        <h1 className="p-7 text-center">Sign-up here</h1>
+        <div className="grid grid-rows-7 items-center">
           <div className="grid grid-flow-row grid-cols-2">
-            <label>First Name:</label>
+            <label className="text-middle content-center">First Name:</label>
             <input className="p-2 m-2 border-2 rounded-lg " name="first-name" id="first-name" />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
-            <label>Surname:</label>
+            <label className="text-middle content-center">Surname:</label>
             <input className="p-2 m-2 border-2 rounded-lg " name="surname" id="surname" />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
-            <label>Email:</label>
+            <label className="text-middle content-center">Email:</label>
             <input className="p-2 m-2 border-2 rounded-lg" name="email" id="email" />{" "}
           </div>
           <div className="grid grid-flow-row grid-cols-2">
-            <label>Password:</label>
-            <input className="p-2 m-2 border-2 rounded-lg " name="password" id="password" />
+            <label className="text-middle content-center">Password:</label>
+            <input className="p-2 m-2 border-2 rounded-lg " name="password" id="password" type="password" />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
-            <label>Confirm Password:</label>
-            <input className="p-2 m-2 border-2 rounded-lg " name="confirm-password" id="confirm-password" />
+            <label className="text-middle content-center">Confirm Password:</label>
+            <input
+              className="p-2 m-2 border-2 rounded-lg "
+              name="confirm-password"
+              id="confirm-password"
+              type="password"
+            />
           </div>
-          <button className="bg-amber-600 rounded-lg p-3 m-2 hover:bg-amber-500">Submit</button>
+          <button className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">Submit</button>
           <Link
-            className="text-centre indent-48 not-last:rounded-lg p-3 m-2 hover:text-blue-300"
+            className="text-center not-last:rounded-lg p-3 m-2 hover:text-blue-300"
             href="http://localhost:3000"
           >
             Log-in{" "}
