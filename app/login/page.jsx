da3f8537c849834a0d6a595c67545d710e4f4f49
aaronbogumil <aaronbogumil@gmail.com>
2025-11-26 15:09:11 +0000

    add handleSumbit function to take values from input fields

---
+++ app/login/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    console.log(email, password);
+  };
+  return (
+    <>
+      <form
+        className="grid-cols shadow-lg rounded-lg p-2 items-center w-150 mx-auto mt-3 max-w-full h-auto bg-white text-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <h1 className="p-7 text-center content-center">Login here</h1>
+        <div className="grid-rows-2 items-center">
+          <div className="grid grid-flow-row grid-cols-2 col grid-rows-1">
+            <label className="indent-20 text-middle content-center">Email:</label>
+            <input
+              type="email"
+              className="p-2 m-1 border-2 rounded-lg text-left"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="grid grid-flow-row grid-cols-2 ">
+            <label className="indent-20 text-middle content-center">Password:</label>
+            <input
+              className="p-2 m-1 border-2 rounded-lg text-left"
+              name="password"
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <button type="submit" className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer ">
+            Login
+          </button>
+          <Link href="http://localhost:3000/sign-up">
+            <button type="button" className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">
+              Create an account
+            </button>
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+}
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,26 +1,64 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = () => {
+    console.log(firstName, surname, email, password, confirmPassword);
+  };
   return (
     <>
-      <form className="grid-cols border-2 border-blue-400 rounded-lg p-2 items-center w-150 mx-auto mt-3 max-w-full h-auto">
+      <form
+        className="grid-cols border-2 border-blue-400 rounded-lg p-2 items-center w-150 mx-auto mt-3 max-w-full h-auto"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
         <h1 className="p-7 text-center">Sign-up here</h1>
         <div className="grid grid-rows-7 items-center">
           <div className="grid grid-flow-row grid-cols-2">
             <label className="text-middle content-center">First Name:</label>
-            <input className="p-2 m-2 border-2 rounded-lg " name="first-name" id="first-name" />
+            <input
+              className="p-2 m-2 border-2 rounded-lg "
+              name="first-name"
+              id="first-name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
             <label className="text-middle content-center">Surname:</label>
-            <input className="p-2 m-2 border-2 rounded-lg " name="surname" id="surname" />
+            <input
+              className="p-2 m-2 border-2 rounded-lg "
+              name="surname"
+              id="surname"
+              onChange={(e) => setSurname(e.target.value)}
+            />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
             <label className="text-middle content-center">Email:</label>
-            <input className="p-2 m-2 border-2 rounded-lg" name="email" id="email" />{" "}
+            <input
+              type="email"
+              className="p-2 m-2 border-2 rounded-lg"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />{" "}
           </div>
           <div className="grid grid-flow-row grid-cols-2">
             <label className="text-middle content-center">Password:</label>
-            <input className="p-2 m-2 border-2 rounded-lg " name="password" id="password" type="password" />
+            <input
+              className="p-2 m-2 border-2 rounded-lg "
+              name="password"
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="grid grid-flow-row grid-cols-2">
             <label className="text-middle content-center">Confirm Password:</label>
@@ -29,13 +67,13 @@ export default function Signup() {
               name="confirm-password"
               id="confirm-password"
               type="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
-          <button className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">Submit</button>
-          <Link
-            className="text-center not-last:rounded-lg p-3 m-2 hover:text-blue-300"
-            href="http://localhost:3000"
-          >
+          <button type="submit" className="bg-blue-400 rounded-lg p-3 m-2 hover:bg-blue-300 hover:cursor-pointer">
+            Submit
+          </button>
+          <Link className="text-center not-last:rounded-lg p-3 m-2 hover:text-blue-300" href="http://localhost:3000">
             Log-in{" "}
           </Link>
         </div>
