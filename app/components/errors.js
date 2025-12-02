e4eb0e7d013a314320ba148fd673358f2c53e378
jamiemarshfeay <jamiecfeay@hotmail.co.uk>
2025-12-02 11:56:07 +0000

    apply prettier to errors component

--- app/components/errors.js
+++ app/components/errors.js
@@ -4,34 +4,32 @@ export default function Error({ message }) {
   return (
     <section className="flex justify-center items-center py-16 px-4 bg-background text-foreground">
       <div className="w-full max-w-md bg-neutral-50 shadow-lg rounded-2xl p-8 border border-neutral-100 font-semibold text-center">
-        <h1 className="font-bold">
-          Something went wrong!
-        </h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="50"
-            height="50"
-            viewBox="0 0 48 48"
-            className="ml-auto mr-auto mt-2 mb-1.5"
-          >
-            <path
-              fill="#f44336"
-              d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
-            ></path>
-            <path
-              fill="#fff"
-              d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828-2.828L29.656,15.516z"
-            ></path>
-            <path
-              fill="#fff"
-              d="M32.484,29.656l-2.828,2.828l-14.14-14.14l2.828-2.828L32.484,29.656z"
-            ></path>
-          </svg>
+        <h1 className="font-bold">Something went wrong!</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="50"
+          height="50"
+          viewBox="0 0 48 48"
+          className="ml-auto mr-auto mt-2 mb-1.5"
+        >
+          <path
+            fill="#f44336"
+            d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+          ></path>
+          <path
+            fill="#fff"
+            d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828-2.828L29.656,15.516z"
+          ></path>
+          <path
+            fill="#fff"
+            d="M32.484,29.656l-2.828,2.828l-14.14-14.14l2.828-2.828L32.484,29.656z"
+          ></path>
+        </svg>
         <h2>
-          Sorry, there was a problem loading{message && (" " + message)}. Please try again in a
-          moment.
+          Sorry, there was a problem loading{message && " " + message}. Please
+          try again in a moment.
         </h2>
       </div>
     </section>
