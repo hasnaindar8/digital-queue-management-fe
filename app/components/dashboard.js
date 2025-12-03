fbe78e323e4feb219adea62f102e6aa3dc53bbc6
Faria Hasan Talukder <faria1136@gmail.com>
2025-12-03 13:42:48 +0000

    Partially done responsive in dashboard UI

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { fetchListOfQueue, deleteQueueEntry } from "../../api.js";
 import { useUser } from "context/UserContext.jsx";
 import Loader from "@/components/loading.js";
+import Link from "next/link.js";
 
 export default function Dashboard() {
   const [queue, setQueue] = useState([]);
@@ -57,70 +58,78 @@ export default function Dashboard() {
           <b>Receptionist Name:</b>{" "}
           {capitalName(user.firstName) + " " + capitalName(user.surname)}
         </h1>
-        <table className="border-separate mx-auto my-4 w-3xl max-w-md bg-neutral-50 shadow-lg rounded-2xl p-6 border-neutral-100">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 p-3 bg-[var(--brand-light)]">
-                Queue Number
-              </th>
+        {/* <Link href="/login">Sign out</Link> */}
+        <div className="flex flex-wrap overflow-x-auto ">
+          <table className="table-auto border-separate mx-auto my-4 w-3xl max-w-md bg-neutral-50 shadow-lg rounded-2xl p-6 border-neutral-100">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-3 bg-brand text-background rounded-tl-2xl font-semibold">
+                  Queue Number
+                </th>
 
-              <th className="border border-gray-300 p-3 bg-[var(--brand-light)]">
-                Patient First Name
-              </th>
-              <th
-                className="border border-gray-300 p-3 bg-[var(--brand-light)]"
-                style={{ backgroundColor: "var(--brand-light)" }}
-              >
-                Patient Surname
-              </th>
-              <th className="border border-gray-300 p-3 bg-[var(--brand-light)]">
-                Patient Phone No.
-              </th>
-              <th className="border border-gray-300 p-3 bg-[var(--brand-light)]">
-                Reason
-              </th>
-              <th className="border border-gray-300 bg-blue-400 p-3 bg-[var(--brand-light)]">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {queue.map((element) => {
-              return (
-                <tr key={element.entry_id}>
-                  <td className="border border-gray-300 p-3">
-                    {element.entry_id}
-                  </td>
+                <th className="border border-gray-300 p-3 bg-brand text-background font-semibold">
+                  Patient First Name
+                </th>
+                <th className="border border-gray-300 p-3 bg-brand text-background font-semibold">
+                  Patient Surname
+                </th>
+                <th className="border border-gray-300 p-3 bg-brand text-background font-semibold">
+                  Patient Phone No.
+                </th>
+                <th className="border border-gray-300 p-3 bg-brand text-background font-semibold">
+                  Reason
+                </th>
+                <th className="border border-gray-300 p-3 bg-brand text-background font-semibold rounded-tr-2xl">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {queue.map((element, index) => {
+                return (
+                  <tr key={element.entry_id}>
+                    <td
+                      className={`border border-gray-300 p-3 ${
+                        index === queue.length - 1 ? "rounded-bl-2xl" : ""
+                      }`}
+                    >
+                      {element.entry_id}
+                    </td>
 
-                  <td className="border border-gray-300 p-3">
-                    {capitalName(element.first_name)}
-                  </td>
-                  <td className="border border-gray-300 p-3">
-                    {capitalName(element.surname)}
-                  </td>
-                  <td className="border border-gray-300 p-3">
-                    {element.phone_no}
-                  </td>
-                  <td className="border border-gray-300 p-3">
-                    {element.label}
-                  </td>
-                  <td className="border border-gray-300 p-3">
-                    {element.status === "done" ? (
-                      "Done"
-                    ) : (
-                      <button
-                        className="bg-blue-300 p-2 rounded-lg hover:bg-blue-500"
-                        onClick={() => removeQueueEntry(element.entry_id)}
-                      >
-                        Delete
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                    <td className="border border-gray-300 p-3">
+                      {capitalName(element.first_name)}
+                    </td>
+                    <td className="border border-gray-300 p-3">
+                      {capitalName(element.surname)}
+                    </td>
+                    <td className="border border-gray-300 p-3">
+                      {element.phone_no}
+                    </td>
+                    <td className="border border-gray-300 p-3">
+                      {element.label}
+                    </td>
+                    <td
+                      className={`border border-gray-300 p-3 ${
+                        index === queue.length - 1 ? "rounded-br-2xl" : ""
+                      }`}
+                    >
+                      {element.status === "done" ? (
+                        "Done"
+                      ) : (
+                        <button
+                          className="h-auto w-32 p-2 rounded-lg bg-brand text-background hover:bg-brand-dark "
+                          onClick={() => removeQueueEntry(element.entry_id)}
+                        >
+                          Mark as Served
+                        </button>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </section>
     </>
   );
