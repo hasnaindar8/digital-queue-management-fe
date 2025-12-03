91c0e2af5dc885f3154a4c83138ca1834af8e543
hasnaindar8 <hasnaindar1999@gmail.com>
2025-12-03 13:52:48 +0000

    integrate socket event for live update of queue and update socket context with useState hook instead of useRef

--- app/queue-position/page.js
+++ app/queue-position/page.js
@@ -1,9 +1,64 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { useSocket } from "@/contexts/socketContext";
+import { useEffect, useState } from "react";
+import Loader from "@/components/loading";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/contexts/userContext";
 
 export default function Page() {
-  const [queuePosition, setQueuePosition] = useState(null);
-  const [estWaitTime, setEstWaitTime] = useState("0 mins");
+  const socket = useSocket();
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [queueUpdate, setQueueUpdate] = useState(null);
+  const [isLeaving, setIsLeaving] = useState(false);
+
+  function handleClick() {
+    setIsLeaving(true);
+
+    fetch(`http://localhost:8080/api/queue/${user?.userId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Something went wrong");
+        router.push("/join-queue");
+      })
+      .catch((err) => {
+        console.error("Unable to leave queue: " + err);
+      })
+      .finally(() => {
+        setIsLeaving(false);
+      });
+  }
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleUpdate = (data) => {
+      console.log(`SOCKET listening on ${socket?.id}`);
+      setQueueUpdate(data);
+    };
+
+    if (socket.connected) {
+      socket.emit("queue:requestUpdate");
+    } else {
+      const onConnect = () => {
+        socket.emit("queue:requestUpdate");
+      };
+      socket.on("connect", onConnect);
+    }
+
+    socket.on("queue:update", handleUpdate);
+
+    return () => {
+      socket.off("queue:update");
+      socket.off("connect");
+    };
+  }, [socket]);
+
+  if (!queueUpdate) {
+    return <Loader />;
+  }
 
   return (
     <div className="flex justify-center items-center py-16 px-4 bg-background text-foreground text-center">
@@ -11,15 +66,20 @@ export default function Page() {
         <h2 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight">
           Where are you in the queue?
         </h2>
-        <section>
-          <h2 className="">Queue Position</h2>
-          {queuePosition || 1}
-        </section>
-        <section>
-          <h2 >Estimated Time</h2>
-          {estWaitTime || "2 mins"}
-        </section>
-        <button className="w-full bg-brand hover:bg-brand-dark text-white font-semibold py-3 rounded-xl transition-all shadow-sm focus:ring-2 focus:ring-brand-light">
+        <h2>Queue Length</h2>
+        <p>{queueUpdate.queueLength}</p>
+
+        <h2>Queue Position</h2>
+        <p>{queueUpdate.position}</p>
+
+        <h2>Estimated Time</h2>
+        <p>{queueUpdate.estimatedWait}</p>
+
+        <button
+          className="w-full bg-brand hover:bg-brand-dark text-white font-semibold py-3 rounded-xl transition-all shadow-sm focus:ring-2 focus:ring-brand-light"
+          onClick={handleClick}
+          disabled={isLeaving}
+        >
           Leave the Queue
         </button>
       </main>
